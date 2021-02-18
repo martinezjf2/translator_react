dcac7a15ae8433e38e2ac651933da0a16d7a8007
Jeffrey Martinez <57267088+martinezjf2@users.noreply.github.com>
2021-02-18 17:21:24 -0500

    used the useState hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./styles.css";
-import React from "react";
+import React, { useState } from "react";
 import Field from "./components/field";
 import Languages from "./components/languages";
 import Translate from "./components/translate";
 
 export default function App() {
+  const [language, setLanguage] = useState("ru");
+  const [text, setText] = useState("");
+
   return (
     <div>
       <Field />
